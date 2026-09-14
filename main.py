eb419cb7747571e0936958f43a5e6d538a5975c5
Nicolas Joaquim <nicolascastrorod@gmail.com>
2026-09-14 18:15:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import discord
+from discord.ext import commands
 
-meme_dict = {
-            "CRINGE": "Algo vergonhoso ou constrangedor",
-            "STALKEAR": "Investigar a vida de alguém online",
-            "VDD": "abreviação da palavra verdade " ,
-            "BISCOITAR": "postar algo apenas para chamar a atenção" 
-            }
-for i in range(5):
-  word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
-if word in meme_dict.keys():
-    print (meme_dict[word])
-else:
-    print ("Nao consta na minha base de dados!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Olá! eu sou um bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(" ")
+O
